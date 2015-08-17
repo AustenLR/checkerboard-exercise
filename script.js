047cd3cf473e74ea8d3a6c087daf800ac73d401a
Austen Lane-Roberts <austen.lane.roberts@gmail.com>
2015-08-17 11:23:19 -0700

    made the code more DRY

--- script.js
+++ script.js
@@ -11,21 +11,15 @@ window.onload = function(){
  // i can keep track of divs
 function checkerboard (){
 	for (var i =1; i < 64; i++){
+		var newElement = document.createElement("div");
+		newElement.style.width = "11.1%";
+		newElement.style.paddingBottom = "11.1%";
+		newElement.style.float ="left";
 		if (i % 2 === 1){
-			var newElement = document.createElement("div")
-			newElement.style.backgroundColor = "black";
-			newElement.style.width = "11.1%";
-			newElement.style.paddingBottom = "11.1%";
-			newElement.style.float ="left";
-			document.getElementsByTagName("body")[0].appendChild(newElement);
-
+			newElement.style.backgroundColor = "black";			
 		} else {
-			var newElement = document.createElement("div")
-			newElement.style.backgroundColor = "red";
-			newElement.style.width = "11.1%";
-			newElement.style.paddingBottom = "11.1%";
-			newElement.style.float ="left";
-			document.getElementsByTagName("body")[0].appendChild(newElement);
+			newElement.style.backgroundColor = "red";		
 		}
+		document.getElementsByTagName("body")[0].appendChild(newElement);
 	}
 }
