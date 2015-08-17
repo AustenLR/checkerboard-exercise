e78eea39d23e0e447b05de02d20fd92c004f2f35
Austen Lane-Roberts <austen.lane.roberts@gmail.com>
2015-08-17 11:11:30 -0700

    completed but doesnt seem DRY

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-// Your JS goes here
+// loop through to keep creating divs (createElement)
+// each loop creates a new div
+//give style 
+
+// var with div count
+	// even and odd divs have different colors
+window.onload = function(){
+	checkerboard();
+}
+
+ // i can keep track of divs
+function checkerboard (){
+	for (var i =1; i < 64; i++){
+		if (i % 2 === 1){
+			var newElement = document.createElement("div")
+			newElement.style.backgroundColor = "black";
+			newElement.style.width = "11.1%";
+			newElement.style.paddingBottom = "11.1%";
+			newElement.style.float ="left";
+			document.getElementsByTagName("body")[0].appendChild(newElement);
+
+		} else {
+			var newElement = document.createElement("div")
+			newElement.style.backgroundColor = "red";
+			newElement.style.width = "11.1%";
+			newElement.style.paddingBottom = "11.1%";
+			newElement.style.float ="left";
+			document.getElementsByTagName("body")[0].appendChild(newElement);
+		}
+	}
+}
